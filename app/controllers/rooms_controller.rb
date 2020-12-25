d1c18cc7c5e94eee8753f8da0dbd8d9fec6b609e
makuto114 <35193508+makuto114@users.noreply.github.com>
2020-12-25 19:07:41 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,5 +4,17 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
