cb5ae2c7d6f4d99d266583d239ceecd23c87fec4
makuto114 <35193508+makuto114@users.noreply.github.com>
2020-12-28 12:48:30 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,23 @@
 class MessagesController < ApplicationController
   def index
-    @user = User.find(current_user.id)
+    @room = Room.find(params[:room_id])
+    @message = Message.new
+
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
